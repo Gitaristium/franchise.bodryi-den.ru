888cdb963ee92e4cb1b00dca3fb5cfaae2ba2ba4
Alexander German <gitaristium@gmail.com>
2023-10-19 14:55:50 +0700

    add: YA Varioqub script; fix: mail pattern

--- app/assets/theme/js/main.js
+++ app/assets/theme/js/main.js
@@ -335,172 +335,167 @@ $(document).ready(function () {
 
 // ввод только цифр в поле input tel
 $(document).ready(function () {
-    // AJAX отправка формы
-    /* Article FructCode.com */
-    $("#btn-ajax__form").click(function (e) {
-        event.preventDefault();
+  $("form").keydown(function (event) {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
 
-        $("#ajax__form input").removeClass("error error-mail");
-        if (
-            $('#ajax__form input[name="user__name"]').val().length > 0 &&
-            $('#ajax__form input[name="user__city"]').val().length > 0 &&
-            $('#ajax__form input[name="user__phone"]').val().length > 0
-        ) {
-            if ($('#ajax__form input[name="user__email"]').val().length > 0) {
-                var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
-                var mail = $('#ajax__form input[name="user__email"]').val();
-                if (mail.search(pattern) == 0) {
-                    sendAjaxForm("result_form", "ajax__form", "send.php");
+  // AJAX отправка формы
+  /* Article FructCode.com */
+  $("#btn-ajax__form").click(function (e) {
+    event.preventDefault();
 
-                    return false;
-                } else {
-                    $('#ajax__form input[name="user__email"]').addClass(
-                        "error-mail"
-                    );
-                }
-            } else {
-                sendAjaxForm("result_form", "ajax__form", "send.php");
+    $("#ajax__form input").removeClass("error error-mail");
+    if (
+      $('#ajax__form input[name="user__name"]').val().length > 0 &&
+      $('#ajax__form input[name="user__city"]').val().length > 0 &&
+      $('#ajax__form input[name="user__phone"]').val().length > 0
+    ) {
+      if ($('#ajax__form input[name="user__email"]').val().length > 0) {
+        var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]{2,6}$/i;
+        var mail = $('#ajax__form input[name="user__email"]').val();
+        if (mail.search(pattern) == 0) {
+          sendAjaxForm("result_form", "ajax__form", "send.php");
 
-                return false;
-            }
+          return false;
         } else {
-            $("#ajax__form input").each(function () {
-                $(this).attr("aria-required") == "true" &&
-                    $(this).val().length == 0 &&
-                    $(this).addClass("error");
-            });
-
-            //.addClass("error");
+          $('#ajax__form input[name="user__email"]').addClass("error-mail");
         }
-    });
+      } else {
+        sendAjaxForm("result_form", "ajax__form", "send.php");
 
-    $("#btn-ajax__modal").click(function (e) {
-        event.preventDefault();
-        $("#ajax__modal input").removeClass("error error-mail");
-        if (
-            $('#ajax__modal input[name="user__name"]').val().length > 0 &&
-            $('#ajax__modal input[name="user__city"]').val().length > 0 &&
-            $('#ajax__modal input[name="user__phone"]').val().length > 0
-        ) {
-            if ($('#ajax__modal input[name="user__email"]').val().length > 0) {
-                var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
-                var mail = $('#ajax__modal input[name="user__email"]').val();
-                if (mail.search(pattern) == 0) {
-                    sendAjaxModal("result_form", "ajax__modal", "send.php");
+        return false;
+      }
+    } else {
+      $("#ajax__form input").each(function () {
+        $(this).attr("aria-required") == "true" &&
+          $(this).val().length == 0 &&
+          $(this).addClass("error");
+      });
 
-                    return false;
-                } else {
-                    $('#ajax__modal input[name="user__email"]').addClass(
-                        "error-mail"
-                    );
-                }
-            } else {
-                sendAjaxModal("result_form", "ajax__modal", "send.php");
+      //.addClass("error");
+    }
+  });
 
-                return false;
-            }
+  $("#btn-ajax__modal").click(function (e) {
+    event.preventDefault();
+    $("#ajax__modal input").removeClass("error error-mail");
+    if (
+      $('#ajax__modal input[name="user__name"]').val().length > 0 &&
+      $('#ajax__modal input[name="user__city"]').val().length > 0 &&
+      $('#ajax__modal input[name="user__phone"]').val().length > 0
+    ) {
+      if ($('#ajax__modal input[name="user__email"]').val().length > 0) {
+        var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]{2,6}$/i;
+        var mail = $('#ajax__modal input[name="user__email"]').val();
+        if (mail.search(pattern) == 0) {
+          sendAjaxModal("result_form", "ajax__modal", "send.php");
+
+          return false;
         } else {
-            $("#ajax__modal input").each(function () {
-                $(this).attr("aria-required") == "true" &&
-                    $(this).val().length == 0 &&
-                    $(this).addClass("error");
-            });
-
-            //.addClass("error");
+          $('#ajax__modal input[name="user__email"]').addClass("error-mail");
         }
+      } else {
+        sendAjaxModal("result_form", "ajax__modal", "send.php");
+
+        return false;
+      }
+    } else {
+      $("#ajax__modal input").each(function () {
+        $(this).attr("aria-required") == "true" &&
+          $(this).val().length == 0 &&
+          $(this).addClass("error");
+      });
+
+      //.addClass("error");
+    }
+  });
+
+  $(".input-box").click(function (e) {
+    $(this).children("input").removeClass("error error-mail");
+  });
+
+  function sendAjaxForm(result_form, ajax__form, url) {
+    $(".input__btn").text("отправляем").prop("disabled", true);
+    $.ajax({
+      url: "send.php",
+      type: "POST", //метод отправки
+      dataType: "html", //формат данных
+      data: $("#" + ajax__form).serialize(), // Сеарилизуем объект
+      success: function (response) {
+        //Данные отправлены успешно
+        $("#sended").addClass("active");
+        setTimeout(() => {
+          $(".input").val("");
+        }, 1000);
+        // ym(87461192, "reachGoal", "send_form");
+        $(".input__btn").text("получить бизнес-план").prop("disabled", false);
+        ym(
+          95060645,
+          "reachGoal",
+          $('#ajax__form input[name="click__ya-target"]').val()
+        );
+      },
+      error: function (response) {
+        // Данные не отправлены
+        alert("error");
+        $(".input__btn").text("получить бизнес-план").prop("disabled", false);
+      },
     });
+  }
+  function sendAjaxModal(result_form, ajax__modal, url) {
+    $(".input__btn").text("отправляем").prop("disabled", true);
+    $.ajax({
+      url: "send.php",
+      type: "POST", //метод отправки
+      dataType: "html", //формат данных
+      data: $("#" + ajax__modal).serialize(), // Сеарилизуем объект
+      success: function (response) {
+        //Данные отправлены успешно
+        $("#popup").addClass("inactive");
 
-    $(".input-box").click(function (e) {
-        $(this).children("input").removeClass("error error-mail");
+        setTimeout(() => {
+          $("#popup").removeClass("active inactive");
+          $("#sended").addClass("active");
+        }, 500);
+
+        setTimeout(() => {
+          $(".input").val("");
+        }, 1000);
+        // ym(87461192, "reachGoal", "send_form");
+        $(".input__btn").text("получить бизнес-план").prop("disabled", false);
+        ym(
+          95060645,
+          "reachGoal",
+          $('#ajax__modal input[name="click__ya-target"]').val()
+        );
+      },
+      error: function (response) {
+        // Данные не отправлены
+        // alert("error");
+        $(".input__btn").text("получить бизнес-план").prop("disabled", false);
+      },
     });
+  }
 
-    function sendAjaxForm(result_form, ajax__form, url) {
-        $(".input__btn").text("отправляем").prop("disabled", true);
-        $.ajax({
-            url: "send.php",
-            type: "POST", //метод отправки
-            dataType: "html", //формат данных
-            data: $("#" + ajax__form).serialize(), // Сеарилизуем объект
-            success: function (response) {
-                //Данные отправлены успешно
-                $("#sended").addClass("active");
-                setTimeout(() => {
-                    $(".input").val("");
-                }, 1000);
-                // ym(87461192, "reachGoal", "send_form");
-                $(".input__btn")
-                    .text("получить бизнес-план")
-                    .prop("disabled", false);
-                ym(
-                    95060645,
-                    "reachGoal",
-                    $('#ajax__form input[name="click__ya-target"]').val()
-                );
-            },
-            error: function (response) {
-                // Данные не отправлены
-                alert("error");
-                $(".input__btn")
-                    .text("получить бизнес-план")
-                    .prop("disabled", false);
-            },
-        });
+  // ввод только цифр в поле input tel
+  var telInput = $('input[type="tel"]');
+
+  for (var i = 0; i < telInput.length; i++) {
+    telInput[i].oninput = function () {
+      dpcmTel(this);
+    };
+  }
+  function dpcmTel(input) {
+    var value = input.value;
+    var re = /[^0-9\-\(\)\+\ ]/gi;
+    if (re.test(value)) {
+      value = value.replace(re, "");
+      input.value = value;
     }
-    function sendAjaxModal(result_form, ajax__modal, url) {
-        $(".input__btn").text("отправляем").prop("disabled", true);
-        $.ajax({
-            url: "send.php",
-            type: "POST", //метод отправки
-            dataType: "html", //формат данных
-            data: $("#" + ajax__modal).serialize(), // Сеарилизуем объект
-            success: function (response) {
-                //Данные отправлены успешно
-                $("#popup").addClass("inactive");
-
-                setTimeout(() => {
-                    $("#popup").removeClass("active inactive");
-                    $("#sended").addClass("active");
-                }, 500);
-
-                setTimeout(() => {
-                    $(".input").val("");
-                }, 1000);
-                // ym(87461192, "reachGoal", "send_form");
-                $(".input__btn")
-                    .text("получить бизнес-план")
-                    .prop("disabled", false);
-                ym(
-                    95060645,
-                    "reachGoal",
-                    $('#ajax__modal input[name="click__ya-target"]').val()
-                );
-            },
-            error: function (response) {
-                // Данные не отправлены
-                // alert("error");
-                $(".input__btn")
-                    .text("получить бизнес-план")
-                    .prop("disabled", false);
-            },
-        });
-    }
-
-    // ввод только цифр в поле input tel
-    var telInput = $('input[type="tel"]');
-
-    for (var i = 0; i < telInput.length; i++) {
-        telInput[i].oninput = function () {
-            dpcmTel(this);
-        };
-    }
-    function dpcmTel(input) {
-        var value = input.value;
-        var re = /[^0-9\-\(\)\+\ ]/gi;
-        if (re.test(value)) {
-            value = value.replace(re, "");
-            input.value = value;
-        }
-    }
+  }
 });
 
 $(document).ready(function () {
@@ -765,39 +760,39 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    if (document.getElementById("formats-slider-main") !== null) {
-        var splide = new Splide("#formats-slider-main", {
-            // padding: { left: 50, right: 100 },
-            focus: "center",
-            gap: "2.8125vw",
-            autoWidth: true,
-            rewind: true,
-            pagination: false,
-            arrows: false,
-            breakpoints: {
-                768: {
-                    gap: "6.25vw",
-                },
-            },
-        });
-        var thumbnails = new Splide("#formats-slider-thumbnail", {
-            autoWidth: true,
-            gap: "3.75vw",
-            rewind: true,
-            pagination: false,
-            isNavigation: true,
-            arrows: false,
-            breakpoints: {
-                768: {
-                    gap: "7.5vw",
-                },
-            },
-        });
+  if (document.getElementById("formats-slider-main") !== null) {
+    var splide = new Splide("#formats-slider-main", {
+      // padding: { left: 50, right: 100 },
+      focus: "center",
+      gap: "2.8125vw",
+      autoWidth: true,
+      rewind: true,
+      pagination: false,
+      arrows: false,
+      breakpoints: {
+        768: {
+          gap: "6.25vw",
+        },
+      },
+    });
+    var thumbnails = new Splide("#formats-slider-thumbnail", {
+      autoWidth: true,
+      gap: "3.75vw",
+      rewind: true,
+      pagination: false,
+      isNavigation: true,
+      arrows: false,
+      breakpoints: {
+        768: {
+          gap: "7.5vw",
+        },
+      },
+    });
 
-        splide.sync(thumbnails);
-        splide.mount();
-        thumbnails.mount();
-    }
+    splide.sync(thumbnails);
+    splide.mount();
+    thumbnails.mount();
+  }
 });
 
 $(document).ready(function () {
--- app/header.php
+++ app/header.php
@@ -54,6 +54,21 @@ use Bitrix\Main\Web\Cookie;
 	<script type="text/javascript" src="https://api-maps.yandex.ru/3.0/?apikey=c6e7159c-1efe-4612-8ca8-9eb9574ae925&lang=ru_RU"></script>
 	<!-- /Yandex.Maps -->
 
+	<!-- Varioqub experiments -->
+	<script type="text/javascript">
+		(function(e, x, pe, r, i, me, nt) {
+			e[i] = e[i] || function() {
+					(e[i].a = e[i].a || []).push(arguments)
+				},
+				me = x.createElement(pe), me.async = 1, me.src = r, nt = x.getElementsByTagName(pe)[0], nt.parentNode
+				.insertBefore(me, nt)
+		})
+		(window, document, 'script', 'https://abt.s3.yandex.net/expjs/latest/exp.js', 'ymab');
+		ymab('metrika.95060645', 'init' /*, {clientFeatures}, {callback}*/ );
+	</script>
+	<!-- /Varioqub experiments -->
+
+
 	<!-- <link rel="preload" as="image" href="<?= SITE_DIR ?>assets/theme/img/logo@2x.webp" /> -->
 	<link rel="icon" href="<?= SITE_DIR ?>assets/theme/img/favicon.ico" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
