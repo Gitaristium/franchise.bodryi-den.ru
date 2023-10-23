077dcd90a9dda2e5b2ee18c778cf9bf2fd7b5873
Alexander German <gitaristium@gmail.com>
2023-10-23 15:53:11 +0700

    fix: text

--- app/sections/franchise-pack.php
+++ app/sections/franchise-pack.php
@@ -1,191 +1,192 @@
 <section id="franchise-pack" class="franchise-pack">
-	<h4 class="title title-line franchise-pack__title">
-		<span class="anim">пакет франшизы</span>
-		<span class="anim">пакет франшизы</span>
-		<span class="anim">пакет франшизы</span>
-		<span class="anim">пакет франшизы</span>
-		<span class="anim">пакет франшизы</span>
-		<span class="anim">пакет франшизы</span>
-		<span class="anim">пакет франшизы</span>
-		<span class="anim">пакет франшизы</span>
-	</h4>
-	<div class="container">
-		<div class="franchise-pack__content">
-			<div class="franchise-pack__lightnings">
-				<img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
-				<img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
-				<img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
-				<img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
-			</div>
-			<div class="franchise-pack__eyes">
-				<img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/eye.svg" alt="lightning" />
-				<img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/eye.svg" alt="lightning" />
-				<img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/eye.svg" alt="lightning" />
-			</div>
-			<img class="franchise-pack__smile" src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/smile.svg" alt="smile" />
-			<div class="franchise-pack__desc">
-				<div class="pack-item show">
-					<h5 class="pack-item__title">бренд</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							378 000 чашек в год
-						</li>
-						<li class="pack-item__list-item">
-							497 млн. руб. оборот сети в год
-						</li>
-						<li class="pack-item__list-item">
-							14 млн. руб. вложений в бренд
-						</li>
-						<li class="pack-item__list-item">
-							паушальный взнос на город, а не на
-							кофейню
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">поддержка</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							личный менеджер
-						</li>
-						<li class="pack-item__list-item">
-							закупки и ассортимент
-						</li>
-						<li class="pack-item__list-item">
-							ценовая политика
-						</li>
-						<li class="pack-item__list-item">
-							технология
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">обучение</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							управлению бизнесом
-						</li>
-						<li class="pack-item__list-item">
-							продажам
-						</li>
-						<li class="pack-item__list-item">
-							работе с персоналом
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">
-						кофейня<br />как часть сети
-					</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							единая программа лояльности
-						</li>
-						<li class="pack-item__list-item">
-							рецептуры, сезонное меню и маркетинговая
-							поддержка
-						</li>
-						<li class="pack-item__list-item">
-							дружелюбное сообщество владельцев бодрых
-							кофеен
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">
-						бизнес-процессы
-					</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							финансовая модель
-						</li>
-						<li class="pack-item__list-item">
-							технология и скрипт продаж
-						</li>
-						<li class="pack-item__list-item">
-							crm-система
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">
-						своя<br />эспрессо-смесь
-					</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							узнаваемый вкус: не мелкий крафт, а
-							качество крупной фабрики
-						</li>
-						<li class="pack-item__list-item">
-							продающая упаковка: для дома и для
-							кофейни
-						</li>
-						<li class="pack-item__list-item">
-							растущий спрос: 30 тонн в 2021, 40 тонн
-							в 2022.
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">локация</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							Комплексный анализ при подборе
-						</li>
-						<li class="pack-item__list-item">
-							ГИС-сервисы для подробной аналитики
-						</li>
-						<li class="pack-item__list-item">
-							Неограниченное количество предложений
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">наставник</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							совместный процесс открытия от и до
-						</li>
-						<li class="pack-item__list-item">
-							выезд на обучение и коммерческое
-							открытие, 3 рабочих дня
-						</li>
-						<li class="pack-item__list-item">
-							ввыводим бизнес на расчетные KPI после
-							открытия
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">персонал</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							помощь в подборе и обучение сотрудника
-						</li>
-						<li class="pack-item__list-item">
-							эффективная система мотивации
-						</li>
-						<li class="pack-item__list-item">
-							правильные договорные отношения
-						</li>
-					</ul>
-				</div>
-				<div class="pack-item show">
-					<h5 class="pack-item__title">свобода</h5>
-					<ul class="pack-item__list">
-						<li class="pack-item__list-item">
-							нет жестких рамок по дизайну кофеен
-						</li>
-						<li class="pack-item__list-item">
-							свободная ценовая политика
-						</li>
-						<li class="pack-item__list-item">
-							свобода в принятии решений по развитию
-							бизнеса
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
+    <h4 class="title title-line franchise-pack__title">
+        <span class="anim">пакет франшизы</span>
+        <span class="anim">пакет франшизы</span>
+        <span class="anim">пакет франшизы</span>
+        <span class="anim">пакет франшизы</span>
+        <span class="anim">пакет франшизы</span>
+        <span class="anim">пакет франшизы</span>
+        <span class="anim">пакет франшизы</span>
+        <span class="anim">пакет франшизы</span>
+    </h4>
+    <div class="container">
+        <div class="franchise-pack__content">
+            <div class="franchise-pack__lightnings">
+                <img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
+                <img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
+                <img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
+                <img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/lightning.svg" alt="lightning" />
+            </div>
+            <div class="franchise-pack__eyes">
+                <img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/eye.svg" alt="lightning" />
+                <img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/eye.svg" alt="lightning" />
+                <img src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/eye.svg" alt="lightning" />
+            </div>
+            <img class="franchise-pack__smile" src="<?= SITE_DIR ?>assets/theme/img/franchise-pack/smile.svg"
+                alt="smile" />
+            <div class="franchise-pack__desc">
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">бренд</h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            2 500 000 чашек в год
+                        </li>
+                        <li class="pack-item__list-item">
+                            497 млн. руб. оборот сети в год
+                        </li>
+                        <li class="pack-item__list-item">
+                            14 млн. руб. вложений в бренд
+                        </li>
+                        <li class="pack-item__list-item">
+                            паушальный взнос на город, а не на
+                            кофейню
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">поддержка</h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            личный менеджер
+                        </li>
+                        <li class="pack-item__list-item">
+                            закупки и ассортимент
+                        </li>
+                        <li class="pack-item__list-item">
+                            ценовая политика
+                        </li>
+                        <li class="pack-item__list-item">
+                            технология
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">обучение</h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            управлению бизнесом
+                        </li>
+                        <li class="pack-item__list-item">
+                            продажам
+                        </li>
+                        <li class="pack-item__list-item">
+                            работе с персоналом
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">
+                        кофейня<br />как часть сети
+                    </h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            единая программа лояльности
+                        </li>
+                        <li class="pack-item__list-item">
+                            рецептуры, сезонное меню и маркетинговая
+                            поддержка
+                        </li>
+                        <li class="pack-item__list-item">
+                            дружелюбное сообщество владельцев бодрых
+                            кофеен
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">
+                        бизнес-процессы
+                    </h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            финансовая модель
+                        </li>
+                        <li class="pack-item__list-item">
+                            технология и скрипт продаж
+                        </li>
+                        <li class="pack-item__list-item">
+                            crm-система
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">
+                        своя<br />эспрессо-смесь
+                    </h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            узнаваемый вкус: не мелкий крафт, а
+                            качество крупной фабрики
+                        </li>
+                        <li class="pack-item__list-item">
+                            продающая упаковка: для дома и для
+                            кофейни
+                        </li>
+                        <li class="pack-item__list-item">
+                            растущий спрос: 30 тонн в 2021, 40 тонн
+                            в 2022.
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">локация</h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            Комплексный анализ при подборе
+                        </li>
+                        <li class="pack-item__list-item">
+                            ГИС-сервисы для подробной аналитики
+                        </li>
+                        <li class="pack-item__list-item">
+                            Неограниченное количество предложений
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">наставник</h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            совместный процесс открытия от и до
+                        </li>
+                        <li class="pack-item__list-item">
+                            выезд на обучение и коммерческое
+                            открытие, 3 рабочих дня
+                        </li>
+                        <li class="pack-item__list-item">
+                            ввыводим бизнес на расчетные KPI после
+                            открытия
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">персонал</h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            помощь в подборе и обучение сотрудника
+                        </li>
+                        <li class="pack-item__list-item">
+                            эффективная система мотивации
+                        </li>
+                        <li class="pack-item__list-item">
+                            правильные договорные отношения
+                        </li>
+                    </ul>
+                </div>
+                <div class="pack-item show">
+                    <h5 class="pack-item__title">свобода</h5>
+                    <ul class="pack-item__list">
+                        <li class="pack-item__list-item">
+                            нет жестких рамок по дизайну кофеен
+                        </li>
+                        <li class="pack-item__list-item">
+                            свободная ценовая политика
+                        </li>
+                        <li class="pack-item__list-item">
+                            свобода в принятии решений по развитию
+                            бизнеса
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
