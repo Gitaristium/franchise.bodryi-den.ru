1e415600f14a373afa87af5d583ac32a6ab1be42
Alexander German <gitaristium@gmail.com>
2023-11-15 15:32:28 +0700

    fix: minor

--- app/modals/burger.php
+++ app/modals/burger.php
@@ -1,31 +1,33 @@
 <div id="burger-menu" class="modal">
-	<div class="overlay"></div>
-	<ul class="nav-list">
-		<li>
-			<a href="tel:83833750024" class="nav-tel">8 (383) 375-00-24</a>
-		</li>
-		<li>
-			<button id="btn-nav-call-burger" class="nav-call get-plan shake-bottom anim anim--active">
-				заказать звонок
-			</button>
-		</li>
-		<li>
-			<a href="#finmodel" class="link nav-link">финансовая модель</a>
-		</li>
-		<li>
-			<a href="#franchise-pack" class="link nav-link">пакет франшизы</a>
-		</li>
-		<li>
-			<a href="#formats" class="link nav-link">форматы кофеен</a>
-		</li>
-		<li>
-			<a href="#cases" class="link nav-link">кейсы</a>
-		</li>
-		<li>
-			<a href="#calculator" class="link nav-link">калькулятор</a>
-		</li>
-		<li>
-			<a href="#stages" class="link nav-link">этапы открытия</a>
-		</li>
-	</ul>
+    <div class="overlay"></div>
+    <ul class="nav-list">
+        <li>
+            <a href="tel:83833750024" class="nav-tel"
+                onclick="ym(95060645,'reachGoal','click-phone-number'); return true;">8
+                (383) 375-00-24</a>
+        </li>
+        <li>
+            <button id="btn-nav-call-burger" class="nav-call get-plan shake-bottom anim anim--active">
+                заказать звонок
+            </button>
+        </li>
+        <li>
+            <a href="#finmodel" class="link nav-link">финансовая модель</a>
+        </li>
+        <li>
+            <a href="#franchise-pack" class="link nav-link">пакет франшизы</a>
+        </li>
+        <li>
+            <a href="#formats" class="link nav-link">форматы кофеен</a>
+        </li>
+        <li>
+            <a href="#cases" class="link nav-link">кейсы</a>
+        </li>
+        <li>
+            <a href="#calculator" class="link nav-link">калькулятор</a>
+        </li>
+        <li>
+            <a href="#stages" class="link nav-link">этапы открытия</a>
+        </li>
+    </ul>
 </div>
--- app/sections/smi.php
+++ app/sections/smi.php
@@ -1,33 +1,37 @@
 <section id="smi" class="smi">
-	<div class="container">
-		<div class="smi__content">
-			<div class="smi__text show">СМИ о нас</div>
-			<ul class="smi__list">
-				<li class="smi__list-item show">
-					<a href="http://www.forbes.ru/rating-photogallery/324391-nakormi-i-napoi-samye-vygodnye-franshizy-v-sfere-obshchepita/photo/5" class="smi__link" target="_blank">
-						<picture>
-							<source srcset="<?= SITE_DIR ?>assets/theme/img/smi/forbes.webp" type="image/webp" />
-							<img class="smi__link-img" src="<?= SITE_DIR ?>assets/theme/img/smi/forbes.png" />
-						</picture>
-					</a>
-				</li>
-				<li class="smi__list-item show">
-					<a href="https://nsk.plus.rbc.ru/news/5e6063127a8aa90395248eff" class="smi__link" target="_blank">
-						<picture>
-							<source srcset="<?= SITE_DIR ?>assets/theme/img/smi/rbk.webp" type="image/webp" />
-							<img class="smi__link-img" src="<?= SITE_DIR ?>assets/theme/img/smi/rbk.png" />
-						</picture>
-					</a>
-				</li>
-				<li class="smi__list-item show">
-					<a href="https://ksonline.ru/361477/bodryj-start-novoj-kofejnoj-otrasli/" class="smi__link" target="_blank">
-						<picture>
-							<source srcset="<?= SITE_DIR ?>assets/theme/img/smi/ks.webp" type="image/webp" />
-							<img class="smi__link-img" src="<?= SITE_DIR ?>assets/theme/img/smi/ks.png" />
-						</picture>
-					</a>
-				</li>
-			</ul>
-		</div>
-	</div>
+    <div class="container">
+        <div class="smi__content">
+            <div class="smi__text show">СМИ о нас</div>
+            <ul class="smi__list">
+                <li class="smi__list-item show">
+                    <a href="http://www.forbes.ru/rating-photogallery/324391-nakormi-i-napoi-samye-vygodnye-franshizy-v-sfere-obshchepita/photo/5"
+                        class="smi__link" target="_blank"
+                        onclick="ym(95060645,'reachGoal','click-press'); return true;">
+                        <picture>
+                            <source srcset="<?= SITE_DIR ?>assets/theme/img/smi/forbes.webp" type="image/webp" />
+                            <img class="smi__link-img" src="<?= SITE_DIR ?>assets/theme/img/smi/forbes.png" />
+                        </picture>
+                    </a>
+                </li>
+                <li class="smi__list-item show">
+                    <a href="https://nsk.plus.rbc.ru/news/5e6063127a8aa90395248eff" class="smi__link" target="_blank"
+                        onclick="ym(95060645,'reachGoal','click-press'); return true;">
+                        <picture>
+                            <source srcset="<?= SITE_DIR ?>assets/theme/img/smi/rbk.webp" type="image/webp" />
+                            <img class="smi__link-img" src="<?= SITE_DIR ?>assets/theme/img/smi/rbk.png" />
+                        </picture>
+                    </a>
+                </li>
+                <li class="smi__list-item show">
+                    <a href="https://ksonline.ru/361477/bodryj-start-novoj-kofejnoj-otrasli/" class="smi__link"
+                        target="_blank" onclick="ym(95060645,'reachGoal','click-press'); return true;">
+                        <picture>
+                            <source srcset="<?= SITE_DIR ?>assets/theme/img/smi/ks.webp" type="image/webp" />
+                            <img class="smi__link-img" src="<?= SITE_DIR ?>assets/theme/img/smi/ks.png" />
+                        </picture>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </section>
